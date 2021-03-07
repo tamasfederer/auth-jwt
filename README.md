914d6a1785e89eb3a8de05a0e8b09d8199561525
unknown <tamas.federer@gmail.com>
2021-03-07 10:01:45 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # auth-jwt
 Simple JWT authentication
+
+Simple JWT authentication tool, based on [https://dzone.com/articles/create-your-jwts-from-scratch]
---
+++ generate_jwt.php
@@ -0,0 +1,36 @@
+<?php
+require 'bootstrap.php';
+
+// get the local secret key
+$secret = getenv('SECRET');
+
+// Create the token header
+$header = json_encode([
+    'typ' => 'JWT',
+    'alg' => 'HS256'
+]);
+
+// Create the token payload
+$payload = json_encode([
+    'user_id' => 1,
+    'role' => 'admin',
+    'exp' => 1593828222
+]);
+
+// Encode Header
+$base64UrlHeader = base64UrlEncode($header);
+
+// Encode Payload
+$base64UrlPayload = base64UrlEncode($payload);
+
+// Create Signature Hash
+$signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, $secret, true);
+
+// Encode Signature to Base64Url String
+$base64UrlSignature = base64UrlEncode($signature);
+
+// Create JWT
+$jwt = $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+
+echo "Your token:\n" . $jwt . "\n";
+?>
